f151f8487db0f90f90b1e79a8f7a30ad770d2782
Alex Vlasov <drumlife182@gmail.com>
2022-11-01 18:18:12 +0100

    Worked with json decoder package

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"net/http"
 	"log"
-	"io"
-	"os"
+	"encoding/json"
+	"fmt"
 )
 
-func main()  {
+type Item struct {
+	Count int
+	Description string
+	Auth string
+	Link string
+}
+
+type Response struct {
+	Entries []struct {
+		Item
+	}
+}
+
+func main() {
 	res, err := http.Get("https://api.publicapis.org/entries")
 
 	if err != nil {
@@ -16,10 +29,15 @@ func main()  {
 	if res.StatusCode != http.StatusOK {
 		log.Fatal(res.Status)
 	}
-
-	_, err = io.Copy(os.Stdout, res.Body)
-
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	r := new(Response)
+
+	err = json.NewDecoder(res.Body).Decode(r)
+
+	for _, child := range r.Entries {
+		fmt.Println(child.Description)
 	}
 }
